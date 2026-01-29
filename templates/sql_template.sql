11852f5dfb307591d0fee4547fbe0ac2907a99e5
diegotita4 <diego.emilio.enriquez.nares@gmail.com>
2026-01-28 23:06:27 -0600

    docs: improve templates formatting

--- templates/sql_template.sql
+++ templates/sql_template.sql
@@ -13,17 +13,14 @@ High-level explanation of the solution logic.
 */
 
 SELECT
-    T1.column_name_1 AS "alias_name_1"
-    , COUNT(T1.column_name_2) AS "alias_name_2"
+    T1.column_name_1 AS [alias_name_1]
+    , COUNT(T1.column_name_2) AS [alias_name_2]
 FROM table_name_1 AS T1
 INNER JOIN table_name_2 AS T2
     ON T1.ID = T2.foreign_ID
-WHERE
-    T1.column_name_1 = some_condition
-GROUP BY
-    T1.column_name_1
-    , T1.column_name_2
-HAVING
-    COUNT(T1.column_name_2) = some_value
+WHERE T1.column_name_1 = some_condition
+GROUP BY T1.column_name_1
+HAVING COUNT(T1.column_name_2) = some_value
 ORDER BY
-    T1.column_name_1 ASC/DESC;
+    T1.column_name_1 ASC
+    , T1.column_name_2 DESC;
